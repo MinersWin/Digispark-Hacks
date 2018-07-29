52ec7b4b46a5524e90022a7737b0af7d4a759648
firebird641 <29713393+firebird641@users.noreply.github.com>
2018-07-29 10:08:17 +0200

    Update open_powershell.ino

--- open_powershell.ino
+++ open_powershell.ino
@@ -1,27 +1,27 @@
 #include "DigiKeyboardDe.h"
 #define KEY_LEFT_ARROW 0x50
 
+int timer = 2000;
+
 void setup() {
   DigiKeyboardDe.update();
   DigiKeyboardDe.sendKeyStroke(0);
   DigiKeyboardDe.delay(1000);
   DigiKeyboardDe.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.print("cmd");
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.print("powershell");
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(1000);
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.sendKeyStroke(KEY_LEFT_ARROW);
-  DigiKeyboardDe.delay(1000);
+  DigiKeyboardDe.delay(timer);
   DigiKeyboardDe.sendKeyStroke(KEY_ENTER);
-  DigiKeyboardDe.delay(1000);
 }
 
 void loop() {
